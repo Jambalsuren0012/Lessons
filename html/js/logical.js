ac0455b68a8e20245e971dc0f7ffaed4804ca67d
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-11-29 16:49:14 +0800

    update

---
+++ html/js/logical.js
@@ -0,0 +1,23 @@
+let weeknumber = 8;
+let weekname;
+
+
+let weekNumber = 1;
+let weekName;
+if (weekNumber == 1) {
+weekName = 'Даваа';
+} else if (weekNumber == 2) {
+weekName = 'Мягмар';
+} else if (weekNumber == 3) {
+weekName = 'Лхагва';
+} else if (weekNumber == 4) {
+weekName = 'Пүрэв';
+} else if (weekNumber == 5) {
+weekName = 'Баасан';
+} else if (weekNumber == 6) {
+weekName = 'Бямба';
+}else if (weekNumber == 7)  {
+weekName = 'Ням';
+} else {
+    weekName = 'Буруу тоо';
+}console.log(weekName)
