f1da4f261e0f8a8f0c6924fb7af14f9b930685a9
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-11-30 13:59:23 +0800

    up

--- js/huvisagch/huvi1.js
+++ js/huvisagch/huvi1.js
@@ -1,18 +1,18 @@
-let  a = 3, b = 7, c = 2, d =4,
+let  a = 3, b = 7, c = 2, d =4
 
-let multiply = 1;
+let multi = 1;
 
 if(a < 5 ) {
-    multiply = multiply * a; // multiply = multiply * a
+    multi = multi * a; // multiply = multiply * a
 }
 if(b < 5 ) {
-    multiply = multiply * b; // multiply = multiply * b
+    multi = multi * b; // multiply = multiply * b
 }
 if(c < 5 ) {
-    multiply = multiply * c; // multiply = multiply * c
+    multi = multi * c; // multiply = multiply * c
 }
 if(d < 5 ) {
-    multiply = multiply * d;  // multiply = multiply * d
+    multi = multi * d;  // multiply = multiply * d
 }
 
-console.log("multiply = ", multiply )
+console.log("multi = ", multi )
