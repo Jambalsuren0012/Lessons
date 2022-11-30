3e88a366aa5b000e273380fae243534c4c1190ee
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-11-30 17:10:39 +0800

    update

---
+++ js/between/between.js
@@ -0,0 +1,12 @@
+let num = prompt("Дурын 2 тоо оруулна уу")
+
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(num));
+  
+  console.log(getRandomInt(num));
