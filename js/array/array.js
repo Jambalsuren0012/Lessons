87c3f70c9436ac697272a90126d060ee49c05ca4
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-05 16:38:59 +0800

    ip

---
+++ js/array/array.js
@@ -0,0 +1,30 @@
+// let arrayofNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11]
+// let  max = 0;
+   
+
+// for(let i = 0; i < arrayofNumbers.length; i++) {
+//     if (max < arrayofNumbers[i]) {
+//         max = arrayofNumbers[i];
+//     }
+// }
+// console.log(`max utga n: ${max}`)
+
+
+
+// // let arrayofNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+// let min = 43;
+// for (let i = 0; i < arrayofNumbers.length; i++) {
+//     if (min > arrayofNumbers[i]) {
+//         min = arrayofNumbers[i];
+//     }
+
+// }
+// console.log(`min utga n; ${min}`);
+// let x=[[1,2,3],[4,5,6],[7,8,9]]
+// for(let i=0; i<x.length; i++){
+//     for(let j=0; j<x[0].length; j++){
+//     console.log(x[i][j]);
+//     }
+//     }
+let now = new Date.getMonth ();
+console.log(now);
