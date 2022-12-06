30f8d3446166fc8f76ec5d55b57836ade3825c29
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-06 17:27:46 +0800

    update

--- js/array/array.js
+++ js/array/array.js
@@ -32,5 +32,39 @@
         //  Function that takes number of rows as input. 
         //  Output is a 2D array with the values.\\
        
-let itCompanies = [Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon];
-console.log(itCompanies);
+// let itCompanies = [Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon];
+// console.log(itCompanies);
+
+
+
+// PASCAL TRIANGLE
+// var numRows = 10,
+// triangle, 
+// start, 
+// stop;
+
+// // N is the no. of rows/tiers 
+// // a is the 2-D array consisting of the row content
+
+// function pascalRecursive(n, a) {
+
+// if (n < 2) return a; 
+
+// var prevRow = a[a.length-1];
+// var curRow = [1];
+
+// for (var i = 1; i < prevRow.length; i++) {
+// curRow[i] = prevRow[i] + prevRow[i-1];
+// }
+// curRow.push(1);
+// a.push(curRow);
+
+// return pascalRecursive(n-1, a); // Call the function recursively
+// }
+
+
+// var triangle = pascalRecursive(numRows, [[1]]);
+
+// for(var i = 0; i < triangle.length; i++)
+// console.log(triangle[i]+"\n");
+//     Let me know in the comment
---
+++ js/function/function.js
@@ -0,0 +1,8 @@
+// function calculate(numbers) {
+//     let sum = 0;
+//     for (const number of numbers) {
+//       sum = sum + number;
+//     }
+//     return sum;
+//   }
+//   calculate([1, 2, 4]); // => 7
