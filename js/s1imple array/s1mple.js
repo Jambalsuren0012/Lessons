ab6b804a806636456b00f5ee5bde04156be2ebaa
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-08 16:51:58 +0800

    update

--- js/s1imple array/s1mple.js
+++ js/s1imple array/s1mple.js
@@ -1,12 +1,12 @@
 const itCompanies =['Facebook', 'Google', 'Microsoft', 'Apple', 'Oracle', 'Amazon'];
 
-// console.log(itCompanies.slice(0, 3));
+console.log(itCompanies.slice(0, 3));
 
-// console.log(itCompanies.slice(-3));
+console.log(itCompanies.slice(-3));
 
-// console.log(itCompanies.shift());
+console.log(itCompanies.shift());
 
-// console.log(itCompanies.pop());
+console.log(itCompanies.pop());
 
 itCompanies.slice(1, 0, 'Instagram');
 console.log(itCompanies);
