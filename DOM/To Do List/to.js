caa3a505b896bb6070fa47bdca35065afb66db2b
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-14 21:32:57 +0800

    update

--- DOM/To Do List/to.js
+++ DOM/To Do List/to.js
@@ -1,15 +1,67 @@
+// const addBtn = document.getElementById("add");
+// const taskList = document.getElementById("tasks");
+// const inputText = document.getElementById("to-do-input");
+
+
+
+
+// const cardItem = (text) =>{ 
+//     const item = `
+//                 <div class="task">
+//                     <input type="text" class="text" readonly value="${text}" />
+//                     <div class="actions">
+//                         <button class="edit"><i class="fas fa-pen"></i></button>
+//                         <button class="done"><i class="fas fa-check"></i></button>
+//                         <button class="delete"><i class="fas fa-trash"></i></button>
+//                     </div>
+//                 </div>`;
+//     return item;
+// }
+
+// const taskAdd = () => {
+//     let text = inputText.value;
+//     if(text === ""){
+//         alert("Ta utga oruulaagui baina");
+//     }else{
+
+//         taskList.innerHTML += cardItem(text);
+//     }
+// }
+
+// addBtn.addEventListener("click",taskAdd);
+// document.addEventListener('keyup',(e)=>{
+//     if(e.key === "Enter"){
+//         taskAdd();
+//     }
+// })
+
+// Ognoo heseg
+  var d = new Date();
+    let curr_date = d.getDate();
+    let curr_month = d.getMonth() + 1; 
+    let curr_year = d.getFullYear();
+    let formattedDate =  curr_year + "-" + curr_month  + "-" + curr_date  + " ";
+document.getElementById("DATE").innerHTML = `Он Сар Өдөр: ${formattedDate}`;
+    
 const addBtn = document.getElementById("add");
 const taskList = document.getElementById("tasks");
 const inputText = document.getElementById("to-do-input");
 
+const ustgah = (e) => {
+    // alert("Ustgagdlaa.");
+    const parent = e.parentNode.parentNode.parentNode;
+    const child = e.parentNode.parentNode;
+    parent.removeChild(child)
+}
+
 const cardItem = (text) =>{ 
-    const item = `
+    const item = `   
                 <div class="task">
                     <input type="text" class="text" readonly value="${text}" />
                     <div class="actions">
                         <button class="edit"><i class="fas fa-pen"></i></button>
                         <button class="done"><i class="fas fa-check"></i></button>
-                        <button class="delete"><i class="fas fa-trash"></i></button>
+                        <button class="delete" onclick="ustgah(this)"><i class="fas fa-trash"></i></button>
                     </div>
                 </div>`;
     return item;
@@ -31,3 +83,5 @@ document.addEventListener('keyup',(e)=>{
         taskAdd();
     }
 })
+
+
