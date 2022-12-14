6a519de8f69e36235706d9c17d274eb0c00b1d8e
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-14 16:36:06 +0800

    update

---
+++ DOM/key/key.js
@@ -0,0 +1,9 @@
+console.log("Event");
+
+document.addEventListener("keyup", (event) => {
+    console.log("keydaragdlaa");
+    if(event.key === "Enter"){
+        console.log("Enter daragdlaa");
+    }
+
+});
