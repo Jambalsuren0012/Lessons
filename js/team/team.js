e1b906b537dbbeb1f5283484bf71eb43385050fa
Jambalsuren0012 <naruto_0212@yahoo.com>
2022-12-25 14:25:08 +0800

    update

---
+++ js/team/team.js
@@ -0,0 +1,10 @@
+let ateam = (a = 96, b = 108, c = 89)
+
+let bteam = (d = 88, f = 91, g = 101)
+
+if((a + b + c)/3 > (d+f+g)/3){
+    console.log(`a team wins`);
+} else {
+    console.log(`b team wins`);
+}
+
